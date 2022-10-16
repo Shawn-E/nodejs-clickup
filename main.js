04a2a23143890d6fbba9a8ccaf4f1e810f75679d
Shawn E <Shawn.Engmann@outlook.com>
2022-10-16 14:11:47 -0400

    Added IntelSense and getTask() and get tasks()

--- main.js
+++ main.js
@@ -11,6 +11,12 @@ class ClickUp {
     })
   }
 
+    /**
+     * Creates a task in a specific list
+     * @param {Number} listId list to create the task in
+     * @param {Object} task_data object including all the values and settings of the task
+     * @returns {Object} returns a object with all the data from the request
+     */
     async createTask(listId, task_data) {
         try {
             const { data } = await this.client.post(`/list/${listId}/task`, {
@@ -22,6 +28,11 @@ class ClickUp {
         }
     }
 
+    /**
+     * deletes a task given the task id
+     * @param {Number} taskId 
+     * @returns {Object} returns a object with all the data from the request
+     */
     async deleteTask(taskId) {
         try {
             const { data } = await this.client.delete(`/task/${taskId}`, { 
@@ -33,6 +44,12 @@ class ClickUp {
         }
     }
 
+    /**
+     * Comments on a task by id
+     * @param {Number} taskId 
+     * @param {Object} comment_data 
+     * @returns {Object} returns a object with all the data from the request
+     */
     async commentOnTask(taskId, comment_data) {
         try {
             const { data } = await this.client.post(`/task/${taskId}/comment`, { 
@@ -44,6 +61,12 @@ class ClickUp {
         }
     }
 
+    /**
+     * Updates a task by the id
+     * @param {Number} taskId 
+     * @param {Object} task_data 
+     * @returns {Object} returns a object with all the data from the request
+     */
     async updateTask(taskId, task_data) {
         try {
             const { data } = await this.client.put(`/task/${taskId}`, { 
@@ -54,6 +77,39 @@ class ClickUp {
             console.log(error.message)
         }
     }
+
+    // 1.1.0
+
+    /**
+     * Grabs a task by task id
+     * @param {Number} taskId the task id to get
+     * @param {Boolean} include_subtasks include the sub task or not 
+     * @returns {Object} returns a object with all the data from the request
+     */
+    async getTask(taskId, include_subtasks) {
+        try {
+            const { data } = await this.client.get(`/task/${taskId}?include_subtasks=${include_subtasks}`)
+            return data;
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+    /**
+     * 
+     * @param {Number} listId 
+     * @param {Object} filters 
+     * @returns 
+     */
+    async getTasks(listId, filters) {
+        const querys = new URLSearchParams(filters).toString();
+        try {
+            const { data } = await this.client.get(`/list/${listId}/task?${querys}`)
+            return data;
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
 
 }
 
